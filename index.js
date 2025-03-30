149b5c67d0a947dbc9b266a00b7ebad9aaef2263
jabbasfeet <key-kun@hotmail.com>
2025-03-30 18:19:14 +0100

    Now added some extra comments and capability for bot to open up a conan exiles server.
    This however is NOT protected the same way as the rust command.
    There's no check to see if the server is already started so multiple
    attempts to start server will break it
    
    This needs fixing.

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on('message', message => {
 
   if(message.content.includes('3080') || message.content.includes('ray tracing') || message.content.includes('raytracing') || message.content.includes('nvidia') || message.content.includes('4k')){
 
-    message.channel.send('<:noosethink:1250437902744424569>' + 'Le Based Ray Tracing!' + '<:noosethink:1250437902744424569>') 
+    message.channel.send('<:noosethink:1250437902744424569>' + 'Le Based Ray Tracing!' + '<:noosethink:1250437902744424569>')
       .then(message.react('1250437902744424569'));
     return;
   }
@@ -45,7 +45,7 @@ client.on('message', message => {
     message.channel.send('A girl.... AND a gamer? Whoa mama! Hummina hummina hummina bazooooooooing! eyes pop out AROOOOOOOOGA! jaw drops tongue rolls out WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF WOOF tongue bursts out of the mouth uncontrollably leaking face and everything in reach WURBLWUBRLBWURblrwurblwurlbrwubrlwburlwbruwrlblwublr tiny cupid shoots an arrow through heart Ahhhhhhhhhhh me lady... heart in the shape of a heart starts beating so hard you can see it through shirt ba-bum ba-bum ba-bum ba-bum ba-bum milk truck crashes into a bakery store in the background spiling white liquid and dough on the streets BABY WANTS TO FUCK inhales from the gas tank honka honka honka honka masturabtes furiously ohhhh my gooooodd~')
     return;
   }
-  
+
   //Gorbo Time everyone, You can't have Henry Cavill if you don't help out and take some Gorbo too.
   if(message.content.includes('gorbo')){
 	  message.channel.send('Gorbo just want to fuck')
@@ -58,7 +58,7 @@ client.on('message', message => {
     return;
   }
 
-  //If I remember correctly this was a bastard to set up and uses regexs to find the vowels and stuff to change them into uwu text. 
+  //If I remember correctly this was a bastard to set up and uses regexs to find the vowels and stuff to change them into uwu text.
   if(command === 'owo'){
 
     const regExsl = /l/g;
@@ -70,13 +70,11 @@ client.on('message', message => {
     message.delete()
       .then(repeatMsg = message.content)
         .then(message.channel.send(repeatMsg.replace(/^!owo/gi, "").replace(regExsl, 'w').replace(regExsr, 'w').replace(regExL, 'W').replace(regExR, 'W') + " OwO"));
-
     return;
     }
 
   //Haha funnie
   if(command === 'creeper'){
-
     message.channel.send('Awwwww Maaaan.');
     return;
   }
@@ -86,23 +84,24 @@ client.on('message', message => {
 	  message.channel.send('This command is working');
 	  return;
   }
-  
+
   //really wanted this to work, wanted it to @ the person who was eepy, can also kick them from chat using this. Could be exploited.
   /*if (command === 'eepy'){
 	  //const user = message.mentions.users.first();
 	  message.channel.send('Oh look ${message.author} is so eeeepy...');
 	  return;
   }*/
-  
+
   //Lines here are being re-used for rust server, Can be used for any type of server. Just requires a new batch to be made.
   if(command === 'startrust' && (message.member.roles.cache.find(r=>r.name==="ruster"))){
 	  if(!rustBool){
 	  const {exec} = require ('child_process');
 	  console.log('Server was started by ' + message.author.username);
-	  message.channel.send('Starting the rust server now. Get ready to Lust in Rust buddy.');
 	  exec('start startRust.bat')
 	  rustBool = true;
 	  client.user.setActivity('Rust Server online?: ' +rustBool);
+	  message.channel.send('Starting the rust server now. Get ready to Lust in Rust buddy.')
+		.then(message.delete());
 	  return;
 	  }
 
@@ -139,14 +138,33 @@ client.on('message', message => {
 
 	  return;
   }
-  else if (command === 'stoprust' && (message.member.roles.cache.find(r=>r.name!=="Creeper"))){ //Using a Strict inequality operand so it checks that the user is NOT a creeper specifically
-      message.channel.send('Sorry buddy you are not a rustard you cannot stop the server.')
-        .then(message.delete());
-      return;
-  }
+
+	else if (command === 'stoprust' && (message.member.roles.cache.find(r=>r.name!=="ruster"))){ //Using a Strict inequality operand so it checks that the user is NOT a creeper specifically
+		message.channel.send('Sorry buddy you are not a rustard you cannot stop the server.')
+		 .then(message.delete());
+		return;
+	}
+
+  //Starting and stopping Conan Server below. Must create new flag as per rust above so we can't start or stop multiple times consecutively
+	if(command === 'startconan' && (message.member.roles.cache.find(r=>r.name==="iphaver"))){
+	  const {exec} = require ('child_process');
+	  console.log('Conan Exiles server was started by ' + message.author.username);
+	  exec('start startconon.bat')
+	  message.channel.send('Starting Conan server now, Give me some time to hang dong before connection')
+		.then(message.delete());
+	  return;
+    }
+
+    if (command === 'stopconan' && (message.member.roles.cache.find(r=>r.name==="iphaver"))){
+		  const {exec} = require ('child_process');
+		  exec('start killConan.bat')
+		  message.channel.send('Conon iz ded')
+		  return;
+	}
 
 // Prototype username maker for server if you run out of ideas you can run it for sime sillies. Want to have adjective wordlist + noun wordlist in the end not just full random. Works as is. Nice exercise. (Test command made Junkie Gorbo)
   if (command === 'newname'){
+	  message.delete()
 	  const wordGenerator = () => {
 		  const wordBank = [
 			'Fuck ',
@@ -162,7 +180,7 @@ client.on('message', message => {
 			'Egg ',
 			'Man '
 		  ]
-		  
+
 		  let result = ''
 		  for (let i = 0; i <= 1; i++){
 			  result = result.concat(wordBank[Math.floor(Math.random() * wordBank.length)])
@@ -170,7 +188,7 @@ client.on('message', message => {
 		  return result
 	  }
 	  console.log('------NEWNAME------ called by ' + message.author.username + ' on the server ' + message.guild.name + ' in channel ' + message.channel.name + ' on ' + message.createdAt);
-	  console.log(wordGenerator());
+	  console.log(wordGenerator())
 	  return;
   }
 
@@ -186,6 +204,7 @@ client.on('message', message => {
       .then(console.log('!ip was called by ' + message.author.username + ' on the server ' + message.guild.name + ' in channel ' + message.channel.name + ' on ' + message.createdAt + '.\nThe current IP is ' + pubIP))
           .then(message.delete());
 
+    // This is some sort of error handling but I would hope I wouldnt require this for the member roles. Will try implement this again so the bot doesnt instantly die on error
     /*process.on('unhandledRejection', error => {
 
       console.error('Unhandled promise rejection:', error);
@@ -203,6 +222,8 @@ client.on('message', message => {
       .then(message.delete());
     return;
   }
+
+  //If none of the commands match any of the statement's above.
   else{
     message.channel.send('I seem to not have understood your command please use !help')
       .then(message.delete());
